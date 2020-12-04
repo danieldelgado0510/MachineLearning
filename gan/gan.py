bacdea361171e9ea6f7abc7622262a7b25c1c7e9
Daniel Delgato <danieldel@ufl.edu>
2020-12-04 14:33:43 -0500

    Updated GAN

--- gan/gan.py
+++ gan/gan.py
@@ -1,146 +1,152 @@
-from numpy import expand_dims
-from numpy import ones
-from numpy import zeros
-from numpy import vstack
-from numpy.random import randn
-from numpy.random import randint
-from keras.datasets.mnist import load_data
-from keras.models import Sequential
-from keras.optimizers import Adam
-from keras.layers import Dense
-from keras.layers import Conv2D
-from keras.layers import Flatten
-from keras.layers import Dropout
-from keras.layers import LeakyReLU
-from keras.layers import Reshape
-from keras.layers import Conv2DTranspose
-from keras.utils.vis_utils import plot_model
-from matplotlib import pyplot
+#Generative Adversarial Network 
+#Implemented using TensorFlow
 
-def define_discriminator(in_shape=(28,28,1)):
-	model = Sequential()
-	model.add(Conv2D(64, (3,3), strides=(2,2), padding='same', input_shape=in_shape))
-	model.add(LeakyReLU(alpha=0.2))
-	model.add(Dropout(0.4))
-	model.add(Conv2D(64, (3,3), strides=(2,2), padding='same'))
-	model.add(LeakyReLU(alpha=0.2))
-	model.add(Dropout(0.4))
-	model.add(Flatten())
-	model.add(Dense(1, activation='sigmoid'))
-	
-	opt = Adam(lr=0.0002, beta_1=0.5)
-	model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
+import tensorflow as tf
+import glob
+import imageio
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import PIL
+from tensorflow.keras import layers
+import time
+
+from IPython import display
+
+(train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
+
+train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
+train_images = (train_images - 127.5) / 127.5
+
+BUFFER_SIZE = 60000
+BATCH_SIZE = 256
+
+train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+
+def make_generator_model():
+	model = tf.keras.Sequential()
+	model.add(layers.Dense(7*7*256, use_bias=False, input_shape=(100,)))
+	model.add(layers.BatchNormalization())
+	model.add(layers.LeakyReLU())
+
+	model.add(layers.Reshape((7,7,256)))
+	assert model.output_shape == (None, 7, 7, 256) # batch size
+
+	model.add(layers.Conv2DTranspose(128, (5,5), strides=(1,1), padding='same', use_bias=False))
+	assert model.output_shape == (None, 7, 7, 128)
+	model.add(layers.BatchNormalization())
+	model.add(layers.LeakyReLU())
+
+	model.add(layers.Conv2DTranspose(64, (5,5), strides=(2,2), padding='same', use_bias=False))
+	assert model.output_shape == (None, 14, 14, 64)
+	model.add(layers.BatchNormalization())
+	model.add(layers.LeakyReLU())
+
+	model.add(layers.Conv2DTranspose(1, (5,5), strides=(2,2), padding='same', use_bias=False, activation='tanh'))
+	assert model.output_shape == (None, 28, 28, 1)
+
 	return model
 
-def load_real_samples():
-	(trainX, _), (_, _) = load_data()
-	X = expand_dims(trainX, axis=-1)
-	X = X.astype('float32')
-	X = X / 255.0
-	return X
+def make_discriminator_model():
+	model = tf.keras.Sequential()
+	model.add(layers.Conv2D(64, (5,5), strides=(2,2), padding='same', input_shape=[28,28,1]))
 
-def generate_real_samples(dataset, n_samples):
-	ix = randint(0, dataset.shape[0], n_samples)
-	X = dataset[ix]
-	y = ones((n_samples, 1))
-	return X, y
+	model.add(layers.LeakyReLU())
+	model.add(layers.Dropout(0.3))
 
-def generate_fake_samples(n_samples):
-	X = rand(28*28*n_samples)
-	X = X.reshape((n_samples,28,28,1))
-	y = zeros((n_samples, 1))
-	return X, y
+	model.add(layers.Conv2D(128, (5,5), strides=(2,2), padding='same'))
+	model.add(layers.LeakyReLU())
+	model.add(layers.Dropout(0.3))
 
-def train_discriminator(model, dataset, n_iter=100, n_batch=256):
-	half_batch = int(n_batch / 2)
-	for i in range(n_iter):
-		X_real, y_real = generate_real_samples(dataset, half_batch)
-		_, real_acc = model.train_on_batch(X_real, y_real)
-		X_fake, y_fake = generate_fake_samples(half_batch)
-		_, fake_acc = model.train_on_batch(X_fake, y_fake)
-		print('>%d real=%.0f%% fake=%.0f%%' % (i+1, real_acc*100, fake_acc*100))
+	model.add(layers.Flatten())
+	model.add(layers.Dense(1))
 
-def define_generator(latent_dim):
-	model = Sequential()
-	n_nodes = 128 * 7 * 7
-	model.add(Dense(n_nodes, input_dim = latent_dim))
-	model.add(LeakyReLU(alpha=0.2))
-	model.add(Reshape((7,7,128)))
-	model.add(Conv2DTranspose(128, (4,4), strides=(2,2), padding='same'))
-	model.add(LeakyReLU(alpha=0.2))
-	model.add(Conv2DTranspose(128, (4,4), strides=(2,2), padding='same'))
-	model.add(LeakyReLU(alpha=0.2))
-	model.add(Conv2D(1, (7,7), activation='sigmoid', padding='same'))
 	return model
 
-def generate_latent_points(latent_dim, n_samples):
-	x_input = randn(latent_dim * n_samples)
-	x_input = x_input.reshape(n_samples, latent_dim)
-	return x_input
+generator = make_generator_model()
 
-def generate_fake_samples(g_model, latent_dim, n_samples):
-	x_input = generate_latent_points(latent_dim, n_samples)
-	X = g_model.predict(x_input)
-	y = zeros((n_samples,1))
-	return X,y
+noise = tf.random.normal([1,100])
+generated_image = generator(noise, training=False)
 
-def define_gan(g_model, d_model):
-	d_model.trainable = False
-	model = Sequential()
-	model.add(g_model)
-	model.add(d_model)
-	opt = Adam(lr=0.0002, beta_1=0.5)
-	model.compile(loss='binary_crossentropy', optimizer=opt)
-	return model
+plt.imshow(generated_image[0,:,:,0], cmap='gray')
+plt.savefig('test.png')
 
-def train_gan(gan_mode, latent_dim, n_epochs=100, n_batch=256):
-	for i in range(n_epochs):
-		x_gan = generate_latent_points(latent_dim, n_batch)
-		y_gan = ones((n_batch, 1))
-		gan_model.train_on_batch(x_gan, y_gan)
+discriminator = make_discriminator_model()
+decision = discriminator(generated_image)
+print(decision)
 
-def summarize_performance(epoch, g_model, d_model, dataset, latent_dim, n_samples=100):
-	X_real, y_real = generate_real_samples(dataset, n_samples)
-	_, acc_real = d_model.evaluate(X_real, y_real, verbose=0)
-	x_fake, y_fake = generate_fake_samples(g_model, latent_dim, n_samples)
-	_, acc_fake = d_model.evaluate(x_fake, y_fake, verbose=0)
-	print('>Accuracy real: %.0f%%, fake: %.0f%%' % (acc_real*100, acc_fake*100))
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 
-def train(g_model, d_model, gan_model, dataset, latent_dim, n_epochs=100, n_batch=256):
-	bat_per_epo = int(dataset.shape[0] / n_batch)
-	half_batch = int(n_batch / 2)
-	for i in range(n_epochs):
-		for j in range(bat_per_epo):
-			X_real, y_real = generate_real_samples(dataset, half_batch)
-			X_fake, y_fake = generate_fake_samples(g_model, latent_dim, half_batch)
-			X, y = vstack((X_real, X_fake)), vstack((y_real, y_fake))
-			d_loss, _ = d_model.train_on_batch(X, y)
-			X_gan = generate_latent_points(latent_dim, n_batch)
-			y_gan = ones((n_batch, 1))
-			g_loss = gan_model.train_on_batch(X_gan, y_gan)
-			print('>%d, %d/%d, d=%.3f, g=%.3f' % (i+1, j+1, bat_per_epo, d_loss, g_loss))
-		if(i+1)%2 == 0:
-			summarize_performance(i, g_model, d_model, dataset, latent_dim)
+def discriminator_loss(real_output, fake_output):
+	real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+	fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+	total_loss = real_loss + fake_loss
+	return total_loss
 
+def generator_loss(fake_output):
+	return cross_entropy(tf.ones_like(fake_output), fake_output)
 
-def save_plot(examples, n):
-	for i in range(n*n):
-		pyplot.subplot(n,n,1+i)
-		pyplot.axis('off')
-		pyplot.imshow(examples[i, :, :, 0], cmap='gray_r')
-	pyplot.savefig('gan.png')
+generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+
+checkpoint_dir = './checkpoints'
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer, discriminator_optimizer=discriminator_optimizer, generator=generator, discriminator=discriminator)
+
+EPOCHS = 50
+noise_dim = 100
+num_examples_to_generate = 16
+
+seed = tf.random.normal([num_examples_to_generate, noise_dim])
+
+@tf.function
+def train_step(images):
+	noise = tf.random.normal([BATCH_SIZE, noise_dim])
+
+	with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+		generated_images=generator(noise, training=True)
+
+		real_output = discriminator(images, training=True)
+		fake_output = discriminator(generated_images, training=True)
+
+		gen_loss = generator_loss(fake_output)
+		disc_loss = discriminator_loss(real_output, fake_output)
+
+	gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
+	gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+
+	generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+	discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
 
 
-latent_dim = 100
-batch_size = 32
-d_model = define_discriminator()
-g_model = define_generator(latent_dim)
-gan_model = define_gan(g_model, d_model)
-dataset = load_real_samples()
-n_epochs = 30
-gan_model.summary()
-train(g_model, d_model, gan_model, dataset, latent_dim, n_epochs, batch_size)
-latent_points = generate_latent_points(100,25)
-X = g_model.predict(latent_points)
-save_plot(X, 5)
-#plot_model(model, to_file='discriminator_plot.png', show_shapes=True, show_layer_names=True)
+def train(dataset, epochs):
+	for epoch in range(epochs):
+		start = time.time()
+
+		for image_batch in dataset:
+			train_step(image_batch)
+
+		display.clear_output(wait=True)
+		generate_and_save_images(generator, epoch+1, seed)
+
+		if(epoch+1)%15 == 0:
+			checkpoint.save(file_prefix = checkpoint_prefix)
+
+		print('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
+
+	display.clear_output(wait=True)
+	generate_and_save_images(generator, epochs, seed)
+
+def generate_and_save_images(model, epoch, test_input):
+	predictions = model(test_input, training=False)
+
+	fig = plt.figure(figsize=(4,4))
+
+	for i in range(predictions.shape[0]):
+		plt.subplot(4,4,i+1)
+		plt.imshow(predictions[i,:,:,0]* 127.5 + 127.5, cmap='gray')
+		plt.axis('off')
+
+	plt.savefig('images/image_at_epoch_{:04d}.png'.format(epoch))
+
+train(train_dataset, EPOCHS)
